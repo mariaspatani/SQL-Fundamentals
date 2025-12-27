9d5be432dc1d24fde81bd32050a9f6c99de83227
Maria S Patani <mariaspatani2028@cs.sjcetpalai.ac.in>
2025-12-28 01:47:40 +0530

    Create Qn1.sql

---
+++ PracticeQns/Qn1.sql
@@ -0,0 +1,9 @@
+/*
+Create a database for your company named XYZ
+step 1: create a table inside the DB to store the employee info(id,name and salary)
+step 2: Add following information in the DB
+    1,"ADAM",25000
+    2,"BOB",30000
+    3,"Alex",40000
+step3: select and view all your table datas
+*/
