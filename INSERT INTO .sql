1b7ea94d659daf56ccbad97aba16303950c1cda2
Maria S Patani <mariaspatani2028@cs.sjcetpalai.ac.in>
2025-12-28 00:52:39 +0530

    Create INSERT INTO .sql

---
+++ INSERT INTO .sql
@@ -0,0 +1,9 @@
+CREATE TABLE student(
+	id INT  PRIMARY KEY,
+    name VARCHAR(50),
+    age INT NOT NULL
+);
+INSERT INTO student VALUES(1,"Maria",19);
+INSERT INTO student VALUES(2,"Meenakshi",20);
+INSERT INTO student VALUES(3,"Elizabeth",18);
+INSERT INTO student VALUES(4,"Anakha",19);
