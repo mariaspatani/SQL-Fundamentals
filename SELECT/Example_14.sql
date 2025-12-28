95f57744b4919e605bc1751a72ecc5c45ff5abc5
Maria S Patani <mariaspatani2028@cs.sjcetpalai.ac.in>
2025-12-29 02:17:38 +0530

    Create student table and select with aliases

---
+++ SELECT/Example_14.sql
@@ -0,0 +1,16 @@
+---SELECT with alias
+CREATE TABLE student (
+    id INT PRIMARY KEY,
+    name VARCHAR(50),
+    age INT,
+    city VARCHAR(30),
+    marks INT
+);
+INSERT INTO student VALUES
+(101, 'Anu', 20, 'Kochi', 85),
+(102, 'Rahul', 22, 'Kottayam', 78),
+(103, 'Merin', 21, 'Ernakulam', 92),
+(104, 'Asha', 19, 'Kochi', 65);
+
+SELECT name AS student_name, marks AS score
+FROM student;
