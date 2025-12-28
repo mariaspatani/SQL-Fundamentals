60a80835869284bf98fc5c28fe9b6fc1ea894ceb
Maria S Patani <mariaspatani2028@cs.sjcetpalai.ac.in>
2025-12-29 01:13:17 +0530

    Create database and employee table with data

--- PracticeQns/Qn1.sql
+++ PracticeQns/Qn1.sql
@@ -7,3 +7,21 @@ step 2: Add following information in the DB
     3,"Alex",40000
 step3: select and view all your table datas
 */
+
+CREATE DATABASE xyz_company;
+USE xyz_company;
+
+CREATE TABLE employee(
+	id INT PRIMARY KEY,
+    name VARCHAR(100),
+    salary INT
+);
+
+INSERT INTO employee
+(id,name,salary)
+ VALUES
+    (1,"ADAM",25000),
+    (2,"BOB",30000),
+    (3,"Alex",40000);
+    
+SELECT * FROM employee;
