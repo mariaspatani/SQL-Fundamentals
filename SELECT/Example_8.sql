43ba825b07e41a14c21df9fec5014d2da4be6cd3
Maria S Patani <mariaspatani2028@cs.sjcetpalai.ac.in>
2025-12-29 02:10:19 +0530

    Create Example_8.sql with student table and queries

---
+++ SELECT/Example_8.sql
@@ -0,0 +1,20 @@
+---SELECT with ORDER BY
+CREATE TABLE student (
+    id INT PRIMARY KEY,
+    name VARCHAR(50),
+    age INT,
+    city VARCHAR(30),
+    marks INT
+);
+INSERT INTO student VALUES
+(101, 'Anu', 20, 'Kochi', 85),
+(102, 'Rahul', 22, 'Kottayam', 78),
+(103, 'Merin', 21, 'Ernakulam', 92),
+(104, 'Asha', 19, 'Kochi', 65);
+
+SELECT * FROM student
+ORDER BY marks DESC;
+
+SELECT * FROM student
+ORDER BY name ASC;
+
