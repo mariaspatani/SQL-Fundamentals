9f849d165c3f1abdb6267946abf4353418aee14f
Maria S Patani <mariaspatani2028@cs.sjcetpalai.ac.in>
2025-12-29 02:12:46 +0530

    Add Example_10.sql with DISTINCT query

---
+++ SELECT/Example_10.sql
@@ -0,0 +1,15 @@
+---SELECT with DISTINCT
+CREATE TABLE student (
+    id INT PRIMARY KEY,
+    name VARCHAR(50),
+    age INT,
+    city VARCHAR(30),
+    marks INT
+);
+INSERT INTO student VALUES
+(101, 'Anu', 20, 'Kochi', 85),
+(102, 'Rahul', 22, 'Kottayam', 78),
+(103, 'Merin', 21, 'Ernakulam', 92),
+(104, 'Asha', 19, 'Kochi', 65);
+
+SELECT DISTINCT city FROM student;
