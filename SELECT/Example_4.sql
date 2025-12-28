21e97b72f9e0eeff54f6aaf06503a406abeecd06
Maria S Patani <mariaspatani2028@cs.sjcetpalai.ac.in>
2025-12-29 02:05:01 +0530

    Add SQL script for student table and queries
    Created a SQL script to define a student table and perform queries using AND/OR conditions.

---
+++ SELECT/Example_4.sql
@@ -0,0 +1,20 @@
+---SELECT with AND / OR
+CREATE TABLE student (
+    id INT PRIMARY KEY,
+    name VARCHAR(50),
+    age INT,
+    city VARCHAR(30),
+    marks INT
+);
+INSERT INTO student VALUES
+(101, 'Anu', 20, 'Kochi', 85),
+(102, 'Rahul', 22, 'Kottayam', 78),
+(103, 'Merin', 21, 'Ernakulam', 92),
+(104, 'Asha', 19, 'Kochi', 65);
+
+SELECT * FROM student
+WHERE city = 'Kochi' AND age > 19;
+
+SELECT * FROM student
+WHERE city = 'Kochi' OR city = 'Ernakulam';
+
