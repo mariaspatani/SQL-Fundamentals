c60586c22aa339bb77fce7e38fc69954d586a305
Maria S Patani <mariaspatani2028@cs.sjcetpalai.ac.in>
2025-12-29 02:18:42 +0530

    Update and rename sql stmt .sql to SELECT stmt .sql

---
+++ SELECT/SELECT stmt .sql
@@ -0,0 +1,17 @@
+SELECT * FROM student;
+
+/*
+| Clause   | Purpose           |
+| -------- | ----------------- |
+| SELECT   | Fetch data        |
+| WHERE    | Filter rows       |
+| ORDER BY | Sort result       |
+| GROUP BY | Group rows        |
+| HAVING   | Filter groups     |
+| DISTINCT | Remove duplicates |
+| LIMIT    | Restrict rows     |
+*/
+/*
+The SELECT statement is used to retrieve data from one or more tables in a 
+database with optional filtering, grouping, and sorting conditions.
+*/
