0a814a14bab1d4e277d1b17ac2e48d96b9978dca
Maria S Patani <mariaspatani2028@cs.sjcetpalai.ac.in>
2026-02-06 23:21:45 +0530

    Update TRUNCATE .sql

--- TRUNCATE .sql
+++ TRUNCATE .sql
@@ -1,1 +1,16 @@
+CREATE TABLE students (
+    student_id INT,
+    name VARCHAR(50),
+    department VARCHAR(30)
+);
+
+INSERT INTO students VALUES (1, 'Anu', 'CSE');
+INSERT INTO students VALUES (2, 'Rahul', 'ECE');
+INSERT INTO students VALUES (3, 'John', 'EEE');
+
+SELECT * FROM students;
+
+TRUNCATE TABLE students;
+
+SELECT * FROM students;
 
