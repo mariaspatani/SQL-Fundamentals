be0d02139c9843bbe6a409c19bdc71cf4d999a33
Maria S Patani <mariaspatani2028@cs.sjcetpalai.ac.in>
2026-05-03 13:25:12 +0530

    Add CROSS JOIN example
    Added CROSS JOIN query example
    
    Co-authored-by: bosscaptain <278311774+bosscaptain@users.noreply.github.com>

--- JOINS/outer_join.sql
+++ JOINS/outer_join.sql
@@ -1,6 +1,5 @@
--- FULL OUTER JOIN Example
+-- CROSS JOIN Example
 
 SELECT Customers.customer_name, Orders.product_name
 FROM Customers
-FULL OUTER JOIN Orders
-ON Customers.customer_id = Orders.customer_id;
+CROSS JOIN Orders;
